844333b790263478df33b7535bf572689491e312
zhicai.guo <zhicai.guo@lavector.com>
2020-11-09 13:15:32 +0800

    2020110913

--- examples/mnli_example/download_bert.py
+++ examples/mnli_example/download_bert.py
@@ -3,5 +3,5 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
-model.save_pretrained('bert-base-uncased')
-tokenizer.save_pretrained('bert-base-uncased')
+model.save_pretrained('bert_model')
+tokenizer.save_pretrained('bert_model')
