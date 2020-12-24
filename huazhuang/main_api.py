42d4aea81111aeafda093ab5fd1addca4c6ea8b6
johnson <johnson@github.com>
2020-12-24 14:54:37 +0800

    2020-12-24_14:54

--- huazhuang/main_api.py
+++ huazhuang/main_api.py
@@ -71,7 +71,7 @@ def load_examples(contents, max_seq_length, tokenizer, label_list):
     all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
     all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
     all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-    all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.float)
+    all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
     dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
     return dataset
 
